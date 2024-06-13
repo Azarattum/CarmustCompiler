cb881014882ce5748b027f4ebc210fd0df3fc337
Azarattum <azarattum@pm.me>
2024-06-13 16:05:55 +0700

    Added execution stage

---
+++ src/executor/mod.rs
@@ -0,0 +1,106 @@
+use crate::compile::CompileError;
+use std::{
+    env::temp_dir,
+    fs::{metadata, remove_file, set_permissions},
+    io::Write,
+    os::unix::fs::PermissionsExt,
+    process::{Command, Stdio},
+};
+
+pub trait Executable {
+    fn execute(&self, filename: &str) -> Result<i32, CompileError>;
+}
+
+impl Executable for String {
+    fn execute(&self, filename: &str) -> Result<i32, CompileError> {
+        let object_file = temp_dir().join("program.tmp.o");
+        let executable_file = filename.replace(".c", "");
+
+        let mut as_process = Command::new("as")
+            .arg("-o")
+            .arg(object_file.clone())
+            .stdin(Stdio::piped())
+            .spawn()
+            .or_else(|_| {
+                Err(CompileError {
+                    message: "Failed to start `as` process!",
+                })
+            })?;
+
+        as_process
+            .stdin
+            .as_mut()
+            .ok_or(CompileError {
+                message: "Failed to open stdin!",
+            })?
+            .write(self.as_bytes())
+            .or_else(|_| {
+                Err(CompileError {
+                    message: "Failed to write to stdin!",
+                })
+            })?;
+
+        let status = as_process.wait().or_else(|_| {
+            Err(CompileError {
+                message: "Failed to wait on `as` process!",
+            })
+        })?;
+
+        if !status.success() {
+            return Err(CompileError {
+                message: "`as` command failed!",
+            });
+        }
+
+        let status = Command::new("ld")
+            .arg(object_file.clone())
+            .arg("-e")
+            .arg("main")
+            .arg("-arch")
+            .arg("arm64")
+            .arg("-o")
+            .arg(executable_file.clone())
+            .status()
+            .or_else(|_| {
+                Err(CompileError {
+                    message: "Failed to execute `ld` command!",
+                })
+            })?;
+
+        if !status.success() {
+            return Err(CompileError {
+                message: "`ld` command failed!",
+            });
+        }
+
+        let mut permissions = metadata(executable_file.clone())
+            .or_else(|_| {
+                Err(CompileError {
+                    message: "Failed to get metadata for output file!",
+                })
+            })?
+            .permissions();
+        permissions.set_mode(0o755);
+        set_permissions(executable_file.clone(), permissions).or_else(|_| {
+            Err(CompileError {
+                message: "Failed to set permissions for output file!",
+            })
+        })?;
+
+        let exec_status = Command::new(executable_file.clone())
+            .status()
+            .or_else(|_| {
+                Err(CompileError {
+                    message: "Failed to execute the output file!",
+                })
+            })?;
+
+        remove_file(object_file).or_else(|_| {
+            Err(CompileError {
+                message: "Failed to remove temporary object file!",
+            })
+        })?;
+
+        Ok(exec_status.code().unwrap_or(0))
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,30 @@
 #![feature(let_chains)]
+#![feature(iter_intersperse)]
 #![feature(type_alias_impl_trait)]
 #![feature(return_position_impl_trait_in_trait)]
 
 mod analyzer;
+mod assembly;
 mod error;
+mod executor;
 mod tokenizer;
 mod translator;
 mod types;
 
 use analyzer::*;
-use anyhow::Result;
+use assembly::*;
 use colored::Colorize;
 use error::*;
+use executor::*;
 use program::Program;
 use std::fs::read_to_string;
 use tokenizer::*;
 use translator::*;
 use types::*;
 
-fn main() -> Result<()> {
+fn main() {
     let path = "./assets/sample.c";
-    let file = read_to_string(path)?;
+    let file = read_to_string(path).expect(&format!("File at {path} does not exist!"));
 
     let tokens: Vec<Token> = file.tokenize().collect();
     println!("{}: {:?}\n", "Tokens".bold().cyan(), tokens);
@@ -34,5 +38,13 @@ fn main() -> Result<()> {
         .unwrap_or_else(|error| error.crash(&file, path));
     println!("{}:\n{:?}\n", "IR".bold().green(), program);
 
-    Ok(())
+    let assembly = program
+        .assemble()
+        .unwrap_or_else(|error| error.crash(&file, path));
+    println!("{}:\n{}\n", "ASM".bold().yellow(), assembly);
+
+    let result = assembly
+        .execute(path)
+        .unwrap_or_else(|error| error.crash(&file, path));
+    println!("{}: {}\n", "Execution Result".bold().blue(), result);
 }
